279ed1effc0624b0fdccc29d7bfc2d1c39707b1d
Nikolai Kononenko <nikolay.kononenko@gmail.com>
2020-01-26 21:20:35 +0200

    flickity  scroll ready

---
+++ src/scripts/reviews.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import Flickity from 'vue-flickity';
+ 
+new Vue({
+    el:".reviews",
+    components: {
+    Flickity
+  },
+  
+  data() {
+    return {
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: false,
+        groupCells: 2,
+        freeScroll: false,
+        contain: true
+        
+        // any options from Flickity can be used
+      }
+    }
+  },
+  
+  methods: {
+    next() {
+      this.$refs.flickity.next();
+      this.checkArrows();
+    },
+    
+    previous() {
+      this.$refs.flickity.previous();
+      this.checkArrows();
+    },
+    checkArrows(){
+        if (this.$refs.flickity.selectedIndex()==0){
+            this.$el.querySelector('.reviews-slider__controls-btn--left').disabled = true;
+        } else if (this.$refs.flickity.selectedIndex() == this.$refs.flickity.slides().length -1){
+            this.$el.querySelector('.reviews-slider__controls-btn--right').disabled = true;
+        } else {
+            this.$el.querySelector('.reviews-slider__controls-btn--left').disabled = false;
+            this.$el.querySelector('.reviews-slider__controls-btn--right').disabled = false;
+        }
+    }
+  }
+});
