9e034d90ee244342a949aad36ebcc31d1188d381
Nikolai Kononenko <nikolay.kononenko@gmail.com>
2020-02-04 23:39:07 +0200

    reviews markupready

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -171,6 +171,101 @@
               button.form__reset-btn(type="reset") Cancel
               button.form__btn-submit.main-btn(type="submit") Submit
 
+    section.works
+      .container
+        .works__preview
+          a.works__add-btn.add-btn--square
+            .works__add-btn-circle
+              .works__add-btn-cross +
+            .works__add-btn-text Add new project
+
+          .works__item
+            .works__image
+              img.works__pic(src="../images/content/prew1.jpg")
+              ul.works__tags
+                li.works__tag HTML
+                li.works__tag JS
+                li.works__tag css
+            .works__desc
+              h3.works__title Project name
+              .works__text Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur dignissimos ducimus ea eaque earum eum impedit ipsam laborum, modi nemo officiis optio possimus recusandae repellendus sunt suscipit tenetur velit!
+              a.works__link www.google.com
+            .works__control
+              button.works__btn.span-btn.span-btn--edit
+                .span-btn__text Edit
+                .span-btn__icon.span-btn__icon--edit
+              button.works__btn.span-btn.span-btn--delete
+                .span-btn__text Delete
+                .span-btn__icon.span-btn__icon--delete
+          .works__item
+            .works__image
+              img.works__pic(src="../images/content/prew1.jpg")
+              ul.works__tags
+                li.works__tag HTML
+                li.works__tag JS
+                li.works__tag css
+            .works__desc
+              h3.works__title Project name
+              .works__text Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur dignissimos ducimus ea eaque earum eum impedit ipsam laborum, modi nemo officiis optio possimus recusandae repellendus sunt suscipit tenetur velit!
+              a.works__link www.google.com
+            .works__control
+              button.works__btn.span-btn.span-btn--edit
+                .span-btn__text Edit
+                .span-btn__icon.span-btn__icon--edit
+              button.works__btn.span-btn.span-btn--delete
+                .span-btn__text Delete
+                .span-btn__icon.span-btn__icon--delete
+          .works__item
+            .works__image
+              img.works__pic(src="../images/content/prew1.jpg")
+              ul.works__tags
+                li.works__tag HTML
+                li.works__tag JS
+                li.works__tag css
+            .works__desc
+              h3.works__title Project name
+              .works__text Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequatur dignissimos ducimus ea eaque earum eum impedit ipsam laborum, modi nemo officiis optio possimus recusandae repellendus sunt suscipit tenetur velit!
+              a.works__link www.google.com
+            .works__control
+              button.works__btn.span-btn.span-btn--edit
+                .span-btn__text Edit
+                .span-btn__icon.span-btn__icon--edit
+              button.works__btn.span-btn.span-btn--delete
+                .span-btn__text Delete
+                .span-btn__icon.span-btn__icon--delete
+
+    section.reviews
+      .container
+        .reviews__header
+          h1.reviews__title.section__title Section "Testimonial"
+        .works__content
+          form.reviews__form
+            .form__header
+              h2.form__title New Testimonial
+            .form__content.form__content--reviews
+              .form__content-left
+                .form__user-pic
+                  .form__user-icon-box
+                    .form__user-icon
+                input.form__file-upload(type="file")
+                button.user-pic-upload-btn Upload photo
+              .form__content-right
+                .form-row--2elements
+                  .form-row
+                    label.form__label Author name
+                      input.form__input(type="text" name="name")
+                  .form-row
+                    label.form__label Occupation
+                      input.form__input(type="text" name="prof")
+                  .form-row
+                    label.form__label Text
+                      input.form__input.form__input--textarea(type="textarea" name="desc")
+            .form__controls
+                  button.form__reset-btn(type="reset") Cancel
+                  button.form__btn-submit.main-btn(type="submit") Submit          
+
+
+
 
 
 
@@ -555,6 +650,7 @@ position: relative;
 }
 
 .works{
+  margin-bottom: 32px;
   &__title{
     margin-bottom: 60px;
   }
@@ -671,6 +767,170 @@ position: relative;
   font-size: 16px;
   font-weight: 400;
   margin-right: 59px;
+}
+
+//-works second section works second section works second section works second section
+
+.works__preview{
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 95%;
+}
+.works__add-btn{
+  margin-bottom: 31px;
+}
+.add-btn--square{
+  width: 32%;
+  background: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.works__add-btn-circle{
+  width: 150px;
+  height: 150px;
+  position: relative;
+  border: 2px solid #ffffff;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+.works__add-btn-cross{
+  font-size: 72px;
+  color: #fff;
+  font-weight: 200;
+}
+
+.works__add-btn-text{
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+.works__item{
+  display: flex;
+  flex-direction: column;
+  width: 32%;
+  padding-bottom: 40px;
+  box-shadow: 0.375rem 0.4375rem 0.3125rem 0 rgba(0,0,0,.07);
+  margin-bottom: 31px;
+}
+.works__image{
+  position: relative;
+  margin-bottom: 40px;
+}
+
+.works__tags{
+  position: absolute;
+  right: 0;
+  bottom: 5px;
+  display: flex;
+  
+}
+
+.works__tag{
+  
+  color: rgba(16, 20, 25, 0.7);
+  font-size: 13px;
+  font-weight: 400;
+  text-transform: uppercase;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: #fff;
+  margin-right: 8px;
+}
+.works__desc{
+  width: 90%;
+  margin: 0 auto;
+  margin-bottom: 46px;
+}
+.works__title{
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 26px;
+  
+}
+
+.works__text{
+  opacity: 0.7;
+  margin-bottom: 26px;
+  line-height: 1.5rem;
+}
+
+.works__link{
+  color: #383bcf;
+  font-size: 16px;
+  font-weight: 400;
+}
+
+.works__control {
+  display: flex;
+  justify-content: space-between;
+  width: 93%;
+  margin: 0 auto;
+}
+
+.span-btn{
+  display: flex;
+  background: transparent;
+  justify-content: space-between;
+}
+.span-btn__text{
+  font-weight: 400;
+  opacity: 0.5;
+  font-size: 16px;
+  font-weight: 400;
+  margin-right: 11px;
+}
+
+.span-btn__icon{
+  height: 17px;
+  width: 17px;
+}
+
+.span-btn__icon--edit{
+  background: svg-load('pencil.svg', fill="#383bcf", height=100%, width=100%);
+}
+.span-btn__icon--delete{
+  background: svg-load('trash.svg', fill="#c92e2e", height=100%, width=100%);
+}
+
+//-reviewsreviewsreviewsreviewsreviewsreviewsreviewsreviewsreviewsreviewsreviews
+
+.form__content--reviews{
+  display: flex;
+  justify-content: space-between;
+}
+
+.form__content--reviews .form__content-left{
+  width: 30%;
+}
+
+.form__content--reviews .form__content-right{
+  width: 68%;
+}
+
+.form__user-pic{
+  background-color: #dee4ed;
+  height:200px;
+  width: 200px;
+  border-radius: 50%;
+}
+
+.form__user-icon-box{
+  height: 200px;
+  width: 200px;
+}
+.form__user-icon {
+  display: block;
+  background: svg-load("user2.svg", fill='#ffffff', height=100%, width=100%);
+  background: cover;
 }
 
 </style>
